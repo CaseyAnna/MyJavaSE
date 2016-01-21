c84467695540f87f4847a0fb8c83099a2080bc14
hb_wang <892917947@qq.com>
2016-01-21 10:46:18 +0800

    Random

--- MathDemo/src/com/java/RandomDemo1.java
+++ MathDemo/src/com/java/RandomDemo1.java
@@ -2,14 +2,20 @@ package com.java;
 
 import java.util.Random;
 
-//Ä¬ÈÏÖÖ×Ó
 public class RandomDemo1 {
-	public static void main(String[] args) {
-		Random r = new Random();
-		
-		for (int i=0; i<10; ++i) {
-			int num = r.nextInt(10)+1;
-			System.out.println(num);
-		}
-	}
+    public static void main(String[] args) {
+        Random r = new Random(4);
+
+        for (int i = 0; i < 10; ++i) {
+            int num = r.nextInt(10) + 1;
+            System.out.println(num);
+        }
+
+        Random r1 = new Random(4);
+
+        for (int i = 0; i < 10; ++i) {
+            int num = r1.nextInt(10) + 1;
+            System.out.println(num);
+        }
+    }
 }
