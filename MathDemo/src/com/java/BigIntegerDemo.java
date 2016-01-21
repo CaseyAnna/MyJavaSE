6e51c3814fc4c3681ee11c8ad3494c012eebacaa
hb_wang <892917947@qq.com>
2016-01-21 10:42:26 +0800

    Random Math.random

--- MathDemo/src/com/java/BigIntegerDemo.java
+++ MathDemo/src/com/java/BigIntegerDemo.java
@@ -3,17 +3,17 @@ package com.java;
 import java.math.BigInteger;
 
 public class BigIntegerDemo {
-	public static void main(String[] args) {
-		BigInteger bi1 = new BigInteger("100");
-		BigInteger bi2 = new BigInteger("50");
-		
-		System.out.println(bi1.add(bi2));
-		System.out.println(bi1.subtract(bi2));
-		System.out.println(bi1.multiply(bi2));
-		System.out.println(bi1.divide(bi2));
-		
-		BigInteger[] biArray = bi1.divideAndRemainder(bi2);
-		System.out.println("ษฬ:"+biArray[0]);
-		System.out.println("ำเสý:"+biArray[1]);
-	}
+    public static void main(String[] args) {
+        BigInteger bi1 = new BigInteger("100");
+        BigInteger bi2 = new BigInteger("50");
+
+        System.out.println(bi1.add(bi2));
+        System.out.println(bi1.subtract(bi2));
+        System.out.println(bi1.multiply(bi2));
+        System.out.println(bi1.divide(bi2));
+
+        BigInteger[] biArray = bi1.divideAndRemainder(bi2);
+        System.out.println("aa:" + biArray[0]);
+        System.out.println("aaa:" + biArray[1]);
+    }
 }
