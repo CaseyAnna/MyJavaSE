b34a30fd12ea5ebadf35e075a045b1ba054ebcf0
hb_wang <892917947@qq.com>
2016-01-21 15:24:19 +0800

    SimpleDateFormat Calendar

--- CalendarProj/src/com/calendar/DayInThisWorld2.java
+++ CalendarProj/src/com/calendar/DayInThisWorld2.java
@@ -6,24 +6,23 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
-
 public class DayInThisWorld2 {
-	public static void main(String[] args) throws ParseException {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("please input your birthday:");
-		String line = sc.nextLine();
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		Date d = sdf.parse(line);
-		
-		long myTime = d.getTime();
-		Calendar rightNow = Calendar.getInstance();
-		long nowTime= rightNow.getTimeInMillis();
-		
-		long time = nowTime-myTime;
-		long day = time/1000/60/60/24;
-		
-		System.out.println("你来到这个世界一共"+day+"天了。");
-	}
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("please input your birthday:");
+        String line = sc.nextLine();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date d = sdf.parse(line);
+
+        long myTime = d.getTime();
+        Calendar rightNow = Calendar.getInstance();
+        long nowTime = rightNow.getTimeInMillis();
+
+        long time = nowTime - myTime;
+        long day = time / 1000 / 60 / 60 / 24;
+
+        System.out.println(day);
+    }
 }
