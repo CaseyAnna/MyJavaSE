718ce39cf87b360d63522014dcbd27b179532b44
hb_wang <892917947@qq.com>
2016-01-22 14:18:41 +0800

    BigInteger

--- XMU_OJ/src/com/xmu/Q1007.java
+++ XMU_OJ/src/com/xmu/Q1007.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 public class Q1007 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+        String str1 = new String("abc");
+        String str2 = "abc";
+        String str3 = new String("abc");
+        String str4 = "abc";
+        System.out.println(str1 == str2); //false 
+        System.out.println(str1.equals(str2)); //true 
+        System.out.println(str1 == str3); //false 
+        System.out.println(str2 == str4); //true
+        //        两种的形式来创建，第一种是用new()来新建对象的，它会在存放于堆中。每调用一次就会创建一个新的对象。  
+        //        而第二种是先在栈中创建一个对String类的对象引用变量str，然后查找栈中有没有存放"abc"，如果没有，则将"abc"存放进栈，并令str指向”abc”，如果已经有”abc” 则直接令str指向“abc”。  
 
 		while (sc.hasNext()) {
 			BigInteger i1 = sc.nextBigInteger();
